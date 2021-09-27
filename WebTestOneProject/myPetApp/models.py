5c09da0c6f57961af328afb30444ee1b21677525
Anzor <anzors.andrejevs@gmail.com>
2021-09-27 16:29:49 +0100

    New class with some code in

--- WebTestOneProject/myPetApp/models.py
+++ WebTestOneProject/myPetApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 class Pet(models.Model):
+#Note choices here is a list of tuples which are data value and display value
+    SEX_CHOICES = [('M','Male'), ('F','Female')]
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=30)
+    breed =models.CharField(max_length=30, blank=True)
+    description = models.TextField()
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=True)
+    age = models.IntegerField()
+    submission_date = models.DateTimeField()
