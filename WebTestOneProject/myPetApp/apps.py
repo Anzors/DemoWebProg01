7f9388af88cec835036f9821b5e89a7fa1faf622
Anzor <anzors.andrejevs@gmail.com>
2021-09-27 16:10:31 +0100

    /up to models

---
+++ WebTestOneProject/myPetApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MypetappConfig(AppConfig):
+    name = 'myPetApp'
---
+++ env_myTestProject/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\anzor\source\repos\WebProgramming\WorkAlong01\DemoWebProg01\env_myTestProject\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
