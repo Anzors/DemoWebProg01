a9a34c74126d283035a9cb18ee479708d396db70
Anzor <anzors.andrejevs@gmail.com>
2021-10-06 15:05:53 +0100

    Now it works! We added pet details(html)

--- WebTestOneProject/myPetApp/admin.py
+++ WebTestOneProject/myPetApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from.models import Pet
 
 # Register your models here.
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    # note pass is just a holder for now
+    list_display = ['name', 'species', 'breed','sex']
+
--- WebTestOneProject/myPetApp/views.py
+++ WebTestOneProject/myPetApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Pet
+
+def pet_detail(request, pet_id):
+    return HttpResponse(F"<h1>Pet number: {pet_id}</h1>")
+
+def home(request):
+    pets = Pet.objects.all()
+    return render(request, "home.html", {"pets":pets,})
